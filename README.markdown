8b27553e3ea893269a53ed5768c793425bcb4d65
teu <pjasiulewicz@gmail.com>
2016-06-09 09:58:58 +0200

    added ability to add by tags

--- README.markdown
+++ README.markdown
@@ -5,14 +5,14 @@ This a continuation of Thomas Verbiscer project https://github.com/tverbiscer/ca
 
 capistrano-asgroup is a [Capistrano](https://github.com/capistrano/capistrano) plugin designed to simplify the
 task of deploying to infrastructure hosted on [Amazon EC2](http://aws.amazon.com/ec2/). It was
-completely inspired by the [capistrano-ec2group](https://github.com/logandk/capistrano-ec2group) and 
+completely inspired by the [capistrano-ec2group](https://github.com/logandk/capistrano-ec2group) and
 [capistrano-ec2tag](https://github.com/douglasjarquin/capistrano-ec2tag) plugins, to which all credit is due.
 
 Both of the prior plugins gave you "a way" to deploy using Capistrano to AWS Auto Scaling groups but both
 required you to do so in a non-straightforward manner by putting your Auto Scaling group in its own
-security group or by providing a unique tag for your Auto Scaling group.  This plugin simply takes the 
+security group or by providing a unique tag for your Auto Scaling group.  This plugin simply takes the
 name of the Auto Scaling group and uses that to find the Auto Scaling instances that it should deploy to.  It will
-work with straight up hand created Auto Scaling groups (exact match of the AS group name) or with 
+work with straight up hand created Auto Scaling groups (exact match of the AS group name) or with
 Cloud Formation created Auto Scaling groups (looking for the name in the Cloud Formation format).
 
 ## Installation
@@ -95,7 +95,7 @@ You would do:
 require 'capistrano/asgroup'
 
 task :production do
-  Asgroup.addInstances("my-asg-name", :web)
+  Capistrano::Asgroup.addInstances("my-asg-name", :web)
   logger.info 'Deploying to the PRODUCTION environment!'
 end
 ```
@@ -106,6 +106,13 @@ In order to deploy through a NAT instance in AWS VPC, you will need the instance
 
 ```ruby
 set :asgroup_use_private_ips, true
+```
+
+### Add by tag
+
+There is a simpler way to add instances - by tag k/v:
+```ruby
+Capistrano::Asgroup.addInstancesByTag("tagname", "tagvalue", {other: params})
 ```
 
 
--- lib/capistrano/asgroup.rb
+++ lib/capistrano/asgroup.rb
@@ -3,51 +3,102 @@ require 'aws-sdk'
 require 'capistrano'
 
 module Capistrano
-    class Asgroup
-        def self.addInstances(which, *args)
-            if nil == fetch(:asgroup_use_private_ips)
-              set :asgroup_use_private_ips, false
-            end
-            @ec2_api = Aws::EC2::Client.new(
-              region: fetch(:aws_region)
-              # credentials from ENV
-            )
+  class Asgroup
+    def initialize
+      if nil == fetch(:asgroup_use_private_ips)
+        set :asgroup_use_private_ips, false
+      end
 
-            @as_api = Aws::AutoScaling::Client.new(region: fetch(:aws_region))
-
-             # Get descriptions of all the Auto Scaling groups
-            @autoScaleDesc = @as_api.describe_auto_scaling_groups
-
-            @asGroupInstanceIds = Array.new()
-            # Find the right Auto Scaling group
-            @autoScaleDesc[:auto_scaling_groups].each do |asGroup|
-                  # Look for an exact name match or Cloud Formation style match (<cloud_formation_script>-<as_name>-<generated_id>)
-                if asGroup[:auto_scaling_group_name] == which or asGroup[:auto_scaling_group_name].scan("{which}").length > 0
-                    # For each instance in the Auto Scale group
-                    asGroup[:instances].each do |asInstance|
-                        @asGroupInstanceIds.push(asInstance[:instance_id])
-                    end
-                end
-            end
-
-             # Get descriptions of all the EC2 instances
-            @ec2DescInst = @ec2_api.describe_instances(instance_ids: @asGroupInstanceIds)
-
-             # figure out the instance IP's
-            @ec2DescInst[:reservations].each do |reservation|
-                #remove instances that are either not in this asGroup or not in the "running" state
-                reservation[:instances].delete_if{ |a| not @asGroupInstanceIds.include?(a[:instance_id]) or a[:state][:name] != "running" }.each do |instance|
-                    puts "Found ASG #{which} Instance ID: #{instance[:instance_id]} in VPC: #{instance[:vpc_id]}"
-                    if true == fetch(:asgroup_use_private_ips)
-                        server(instance[:private_ip_address], *args)
-                    else
-                        server(instance[:public_ip_address], *args)
-                    end
-
-                end
-            end
-
-       end
+      @ec2_api ||= Aws::EC2::Client.new(
+        region: fetch(:aws_region)
+        # credentials from ENV
+      )
     end
-end
 
+    # Adds capistrano servers based on instnce tag k/v
+    def self.addInstancesByTag(tagName, tagValue, *args)
+      @instance ||= new
+      @instance.addInstancesByTag(tagName, tagValue, *args)
+    end
+
+    def addInstancesByTag(tagName, tagValue, *args)
+      if nil == fetch(:asgroup_use_private_ips)
+        set :asgroup_use_private_ips, false
+      end
+
+      @ec2_api = Aws::EC2::Client.new(
+        region: fetch(:aws_region)
+        # credentials from ENV
+      )
+      @ec2DescInst = @ec2_api.describe_instances(filters:[
+        name: "tag:#{tagName}", values: [tagValue]
+      ])
+
+      @ec2DescInst[:reservations].each do |reservation|
+        #remove instances that are either not in this asGroup or not in the "running" state
+        reservation[:instances].delete_if{ |a| a[:state][:name] != "running" }.each do |instance|
+          puts "Found tagged #{tagName}:#{tagValue} instance, ID: #{instance[:instance_id]} in VPC: #{instance[:vpc_id]}"
+          if true == fetch(:asgroup_use_private_ips)
+            server(instance[:private_ip_address], *args)
+          else
+            server(instance[:public_ip_address], *args)
+          end
+
+        end
+      end
+    end
+    # Adds capistrano servers based on the given (part of) name of an AWS autoscaling group
+    # Only selecs instances that re in "running" state, ignoring starting up and terminating instances
+    # Params:
+    # +which+:: part or full name of the autoscaling group
+    # +*args+:: argments passed to Capistrano::server method
+    def self.addInstances(which, *args)
+      @instance ||= new
+      @instance.addInstances(which, *args)
+    end
+
+    def addInstances(which, *args)
+      if nil == fetch(:asgroup_use_private_ips)
+        set :asgroup_use_private_ips, false
+      end
+
+      @ec2_api = Aws::EC2::Client.new(
+        region: fetch(:aws_region)
+        # credentials from ENV
+      )
+      @as_api = Aws::AutoScaling::Client.new(region: fetch(:aws_region))
+
+      # Get descriptions of all the Auto Scaling groups
+      @autoScaleDesc = @as_api.describe_auto_scaling_groups
+
+      @asGroupInstanceIds = Array.new()
+      # Find the right Auto Scaling group
+      @autoScaleDesc[:auto_scaling_groups].each do |asGroup|
+        # Look for an exact name match or Cloud Formation style match (<cloud_formation_script>-<as_name>-<generated_id>)
+        if asGroup[:auto_scaling_group_name] == which or asGroup[:auto_scaling_group_name].scan("{which}").length > 0
+          # For each instance in the Auto Scale group
+          asGroup[:instances].each do |asInstance|
+            @asGroupInstanceIds.push(asInstance[:instance_id])
+          end
+        end
+      end
+
+      # Get descriptions of all the EC2 instances
+      @ec2DescInst = @ec2_api.describe_instances(instance_ids: @asGroupInstanceIds)
+      # figure out the instance IP's
+      @ec2DescInst[:reservations].each do |reservation|
+        #remove instances that are either not in this asGroup or not in the "running" state
+        reservation[:instances].delete_if{ |a| not @asGroupInstanceIds.include?(a[:instance_id]) or a[:state][:name] != "running" }.each do |instance|
+          puts "Found ASG #{which} Instance ID: #{instance[:instance_id]} in VPC: #{instance[:vpc_id]}"
+          if true == fetch(:asgroup_use_private_ips)
+            server(instance[:private_ip_address], *args)
+          else
+            server(instance[:public_ip_address], *args)
+          end
+
+        end
+      end
+    end
+
+  end
+end
