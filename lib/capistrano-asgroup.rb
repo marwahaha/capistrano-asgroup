b8ba7279987e52ae613afbb914c8688cb489e04c
teu <pjasiulewicz@gmail.com>
2016-08-30 09:52:35 +0200

    changing the way files are included

--- lib/capistrano-asgroup.rb
+++ lib/capistrano-asgroup.rb
@@ -1,7 +1,5 @@
-load File.expand_path('../capistrano/asgroup.rb', __FILE__)
-
 if Gem::Specification.find_by_name('capistrano').version >= Gem::Version.new('3.0.0')
-  load File.expand_path('capistrano/asgroup.rb', __FILE__)
+  load File.expand_path('../capistrano/asgroup.rb', __FILE__)
 else
   puts "Oops, you may need capistrano 3+"
 end
